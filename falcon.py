c94220c54cafa04126b0bb5e1df430d08bb88f03
Rex <rexx0520@gmail.com>
2018-01-31 14:53:00 +0800

    Flask 和 Falcon 的 API Server 支援

---
+++ falcon.py
@@ -0,0 +1,28 @@
+import TLHCore
+import HCHScore
+import CHGSHore
+import falcon
+import json
+
+def string_to_dict(data):
+    _x = {}
+    for i in data.split('&'):
+        a = i.split('=')
+        _x.update({a[0]:a[1]})
+    return _x
+
+class School:
+    def __init__(self, lib):
+        self.lib=lib
+    def on_post(self, req, resp):
+        client_data = string_to_dict(req.stream.read().decode('utf-8'))
+        try:
+            server_data = self.lib.get(client_data['account'], client_data['password'], client_data['mode'])
+        except ValueError:
+            resp.body = 'Account or password Error!'
+        resp.body = server_data if isinstance(server_data, str) else json.dumps(server_data, ensure_ascii=False)
+
+api = falcon.API()
+api.add_route('/HCHS', School(HCHScore))
+api.add_route('/TLHC', School(TLHCore))
+api.add_route('/CHGSH', School(CHGSHore))
